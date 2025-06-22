f6d189122a3ca32328068892a2fea1318aac3603
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-06-22 16:24:28 -0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+const double = document.querySelector(".main2")
+
+function clicar() {
+        double.innerHTML += `<div class="container">
+               <textarea placeholder="Empty Note" class="empty"></textarea>
+            </div>`
+}
