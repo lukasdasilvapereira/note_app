82461a47c9a697e536c9a4cc061b0ef013543bd5
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-06-23 14:13:40 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const double = document.querySelector(".main2")
+const double = document.querySelector(".main2");
 
 function clicar() {
-        double.innerHTML += `<div class="container">
-               <textarea placeholder="Empty Note" class="empty"></textarea>
-            </div>`
-}
+    double.innerHTML += `
+        <div class="container">
+            <textarea placeholder="Empty Note" class="empty"></textarea>
+        </div>
+    `;
+        }
+
+// Escuta todos os cliques dentro de .main2 (inclusive dos que forem criados depois)
+double.addEventListener("dblclick", function(e) {
+    const container = e.target.closest(".container");
+    if (container && double.contains(container)) {
+        container.remove();
+    }
+});
+
